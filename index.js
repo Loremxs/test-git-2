b1d14f2a51086cb8d4fc524dc107c80b19c30aad
Loremxs <burkeev.valera03@mail.ru>
2023-10-15 17:55:18 +0300

    merge branches

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 console.log('Hello world')
 
-<<<<<<< HEAD
 const userAge = 35
 
 if(userAge >= 18) {
     console.log('Вы совершеннолетний')
 } else {
     console.log('Вы не совершеннолетний')
-=======
-const age = 20
-if(age >= 18) {
-    console.log('Вы совершенолетний')
-} else {
-    console.log('Вы не совершенолетний')
->>>>>>> feature/main-page/user-access
 }
